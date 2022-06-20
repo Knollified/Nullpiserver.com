63b474b9620ab99a433f9effffcac7857a629293
Justin Torres <justin.torres811@gmail.com>
2022-06-19 21:03:50 -0400

    Contact Section added

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ const App = () => {
       <About />
       <Skills/>
       <Projects />
+      <Contact />
     </div>
   );
 }
---
+++ src/containers/contact/Contact.jsx
@@ -0,0 +1,52 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import './Contact.css'
+
+const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('gmail', 'template_9fetzh5', form.current, 'S3l9vmOC9MJrAeNtL')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        form.target.reset();
+    };
+
+    return (
+        <div className='nullpi__contact section__margin'>
+
+            <div className="nullpi__contact-content">
+                <div className="nullpi__contact-text">
+                    <h1 className='gradient__text'>Contact</h1>
+                </div>
+            </div>
+
+            <div className='nullpi__contact-form'>
+                <form ref={form} onSubmit={sendEmail}>
+                    <div className='nullpi__contact-form__label'>
+                        <label>Name: </label>
+                        <input type="text" name="user_name" />
+                    </div>
+                    <div className='nullpi__contact-form__label'>
+                        <label>Email: </label>
+                        <input type="email" name="user_email" />
+                    </div>
+                    <div className='nullpi__contact-form__label'>
+                        <label>Message: </label>
+                        <textarea name="message" />
+                        <input type="submit" value="Send" />
+                    </div>
+
+                </form>
+            </div>
+
+        </div>
+    );
+};
+
+export default Contact;
